98c9c2b1c83272dea3f8ba9533174043c6b9fcbe
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2022-08-02 10:32:52 +0100

    using redux toolkit for state management in a react restaurant app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Cake } from "./features/cake/Cake"
+import { Burger } from "./features/burger/Burger"
+import { NavBar } from "./features/searchValue/NavBar"
+import { User } from "./features/user/User"
+import { useState } from "react"
+
+function App() {
+
+  return (
+ 
+    <>
+      <h2>My Restaurant</h2>
+      <Cake />
+      <Burger />
+      {/* <NavBar />
+      <User /> */}
+    </>
+  );
+}
+
+export default App;
